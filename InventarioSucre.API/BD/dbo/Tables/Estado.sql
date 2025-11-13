bd75af8c5c39510caa0efeb67471799e16b9413b
sebasraven <ssb2841@outlook.es>
2025-11-13 00:23:16 -0600

    Creando BD

---
+++ InventarioSucre.API/BD/dbo/Tables/Estado.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Estado] (
+    [Id]           UNIQUEIDENTIFIER NOT NULL,
+    [NombreEstado] VARCHAR (50)     NOT NULL,
+    CONSTRAINT [PK_Estado] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ InventarioSucre.API/BD/dbo/Tables/Inventario.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Inventario] (
+    [Id]                           UNIQUEIDENTIFIER NOT NULL,
+    [NumeroIdentificacion]         VARCHAR (50)     NULL,
+    [Descripcion]                  VARCHAR (200)    NOT NULL,
+    [Marca]                        VARCHAR (100)    NULL,
+    [Modelo]                       VARCHAR (100)    NULL,
+    [Serie]                        VARCHAR (100)    NULL,
+    [IdEstado]                     UNIQUEIDENTIFIER NOT NULL,
+    [Ubicacion]                    VARCHAR (100)    NOT NULL,
+    [ModoAdquisicion]              VARCHAR (100)    NOT NULL,
+    [Observaciones]                VARCHAR (MAX)    NULL,
+    [ObservacionesInstitucionales] VARCHAR (MAX)    NULL,
+    CONSTRAINT [PK_Inventario] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Inventario_Estado] FOREIGN KEY ([IdEstado]) REFERENCES [dbo].[Estado] ([Id])
+);
+
